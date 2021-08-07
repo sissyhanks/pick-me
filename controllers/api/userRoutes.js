901467119f108f31b3266966bd9e521e0ce41f88
Bernadette McKnight <sissyhanks@yahoo.com>
2021-08-06 20:03:59 -0400

    evend handlers added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,4 +22,43 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if(!dbUserData) {
+      res
+        .status(400)
+        .json({message: 'Are you sure you\'ve been picked before?' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res .status(200)
+        .status(200)
+        .json({ user: dbUserData, message: 'you\'ve been picked!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+      console.log("you're out");
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
